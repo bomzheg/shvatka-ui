023e3009032e1dc8ff8f6bdc5f5613e5afcb518c
ychebyshev <ychebyshev@solit-clouds.ru>
2024-02-04 20:04:07 +0300

    added games list

--- src/app/games/game.service.ts
+++ src/app/games/game.service.ts
@@ -1,18 +1,34 @@
 import {Injectable} from '@angular/core';
 import {HttpAdapter} from "../http.adapter";
 
-export class Game {
+export class Page<T> {
+  constructor(public content: [T]) {
+  }
+}
 
+export class Game {
+  constructor(
+    public id: number,
+    public name: string,
+    public number: number,
+  ) {
+  }
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
+  get games(): [Game] | undefined {
+    return this._games;
+  }
+  private _games: [Game] | undefined
 
   constructor(private http: HttpAdapter) { }
 
-  getGamesList() {
-    return this.http.get("/games")
+  loadGamesList() {
+    return this.http.get<Page<Game>>("/games").subscribe(async r => {
+      this._games = r.content;
+    })
   }
 }
--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,12 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Game, GameService} from "./game.service";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-games',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './games.component.html',
   styleUrl: './games.component.scss'
 })
-export class GamesComponent {
+export class GamesComponent implements OnInit {
+  constructor(private gameService: GameService) {
+  }
+
+  getGames(): [Game] {
+    return this.gameService.games!;
+  }
+
+  ngOnInit(): void {
+        this.gameService.loadGamesList();
+    }
 
 }
