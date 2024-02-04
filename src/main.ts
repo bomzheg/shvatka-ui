cc3659ad9799797558f728df97520cf2d4a591de
ychebyshev <ychebyshev@solit-clouds.ru>
2024-02-04 23:32:19 +0300

    try to fix base href

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {appConfig} from './app/app.config';
+import {AppComponent} from './app/app.component';
+import {environment} from "./environments/environment";
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+declare var  __webpack_public_path__: string;
+__webpack_public_path__ = environment.baseHref;
