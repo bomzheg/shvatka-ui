ba04f7116f36fa0fcd714a6671df5eb5600154b1
ychebyshev <ychebyshev@solit-clouds.ru>
2024-02-06 00:04:10 +0300

    finally fixed deploy

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: "https://nemesis.bomzheg.dev/shvatka_test/api",
+  apiUrl: "https://nemesis.bomzheg.dev/shvatka_dev/api",
   botUsername: "shvatkatestbot",
   baseHref: "/",
 };
