d7798c648cee8f602c0c881bce3340315c68e71e
ychebyshev <ychebyshev@solit-clouds.ru>
2024-02-06 23:16:36 +0300

    more info to game card

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -24,4 +24,5 @@ export class GameComponent implements OnInit {
       return this.gameService.getGame();
     }
 
+  protected readonly JSON = JSON;
 }
--- src/app/game/game.service.ts
+++ src/app/game/game.service.ts
@@ -1,6 +1,43 @@
 import {Injectable} from '@angular/core';
 import {HttpAdapter} from "../http.adapter";
 
+export class HintPart {
+  constructor(
+    public text: string,
+  ) {
+  }
+}
+
+export class TimeHint {
+  constructor(
+    public time: number,
+    public hint: [HintPart],
+  ) {
+  }
+}
+
+export class Scenario {
+  constructor(
+    public id: string,
+    public time_hints: [TimeHint],
+    public keys: [string],
+    public bonus_keys: [any],
+  ) {
+  }
+}
+
+export class Level {
+  constructor(
+    public db_id: number,
+    public name_id: string,
+    public author: any,
+    public scenario: Scenario,
+    public game_id: number | undefined,
+    public number_in_game: number | undefined,
+  ) {
+  }
+}
+
 export class FullGame {
 constructor(
   public id: number ,
@@ -8,7 +45,7 @@ constructor(
   public name: string,
   public status: string,
   public start_at: string | undefined,
-  public levels: [any],
+  public levels: [Level],
 ) {}
 }
 
