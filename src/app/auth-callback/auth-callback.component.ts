0c1419022e407e09acb1a11feb15694facb37287
ychebyshev <ychebyshev@solit-clouds.ru>
2024-03-09 15:22:56 +0300

    added routing after tg login

--- src/app/auth-callback/auth-callback.component.ts
+++ src/app/auth-callback/auth-callback.component.ts
@@ -1,27 +1,28 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, convertToParamMap} from "@angular/router";
+import {ActivatedRoute, convertToParamMap, Router} from "@angular/router";
 import {AuthCallbackService, UserTgAuth} from "./auth-callback.service";
 
 @Component({
   selector: 'app-auth-callback',
   standalone: true,
   imports: [],
-  template: '<div></div>',
+  template: '<div>Входим, подождите...</div>',
 })
 export class AuthCallbackComponent implements OnInit{
   public constructor(
     private activatedRoute: ActivatedRoute,
     private authCallbackService: AuthCallbackService,
+    private router: Router,
   ) {
 
   }
     ngOnInit(): void {
-      this.activatedRoute.queryParams.subscribe(params => {
-        console.log(params);
+      this.activatedRoute.queryParams.subscribe(async params => {
         let paramMap = convertToParamMap(params);
         const user = UserTgAuth.fromParams(paramMap);
         console.log(user)
-        this.authCallbackService.authenticate(user)
+        this.authCallbackService.authenticate(user);
+        await this.router.navigate(["/"])
       });
     }
 
