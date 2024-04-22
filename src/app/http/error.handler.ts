b66de591c59db80ac20808c01a04481ec7e4f519
ychebyshev <ychebyshev@solit-clouds.ru>
2024-04-22 23:21:56 +0300

    fixed multiple matsnakebars

--- src/app/http/error.handler.ts
+++ src/app/http/error.handler.ts
@@ -1,5 +1,6 @@
 import {ErrorHandler, Injectable, NgZone} from "@angular/core";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,17 @@ export class GlobalErrorHandler implements ErrorHandler {
 
   handleError(error: any): void {
     this._zone.run(() => {
-      console.log(JSON.stringify(error));
-      this._snackBar.open("Какая-то сетевая(?) ошибка=(", 'Закрыть', { duration: 3000 });
+      if (!(error instanceof HttpErrorResponse)) {
+        console.error(error)
+        return
+      }
+      if (error.status === 401) {
+        console.log("401 response: " + JSON.stringify(error));
+        this._snackBar.open("Для выполнения этой операции необходимо залогиниться", 'Закрыть', {duration: 3000});
+      } else {
+        console.error(error);
+        this._snackBar.open("Какая-то сетевая(?) ошибка=(", 'Закрыть', {duration: 3000});
+      }
     });
   }
 }
