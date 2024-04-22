2070fa27b3aba9a5a3b61d3ec36a9088a2f8a7d1
ychebyshev <ychebyshev@solit-clouds.ru>
2024-04-22 22:25:39 +0300

    delete unused snackbar

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,4 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {MatSnackBar} from "@angular/material/snack-bar";
 import {AuthService} from "./auth.service";
 import {FormsModule} from "@angular/forms";
 import {NgClass, NgIf, NgStyle} from "@angular/common";
@@ -28,20 +27,15 @@ export class AuthComponent implements AfterViewInit, OnInit {
     public authService: AuthService,
     private userService: UserService,
     private config: ShvatkaConfig,
-    private snackBar: MatSnackBar,
   ) {
     authService.registerCallback(this);
   }
 
   login(username: string | undefined, password: string | undefined) {
-    try {
       this.authService.login(username!, password!)
         .subscribe(() => {
           this.updateUser().then(() => this.closeLoginForm());
         });
-    } catch (e) {
-      this.snackBar.open('Ошибка логина' + e, "ok");
-    }
   }
 
   closeLoginForm() {
