26180e0ae970998ec51a44bd1d85b59079e16ca7
Yuriy Chebyshev <bomzheg@gmail.com>
2026-04-06 23:06:50 +0300

    add proxy for local develop

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   mainUrl: "http://localhost:4200",
-  apiUrl: "http://localhost:8000/api",
+  apiUrl: "http://localhost:4200/api",
   botUsername: "shvatkatestbot",
   baseHref: "/",
 };
--- src/environments/environment.mock.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  mainUrl: "http://localhost:4200",
-  apiUrl: "http://localhost:8000/api",
-  botUsername: "shvatkatestbot",
-  baseHref: "/",
-};
