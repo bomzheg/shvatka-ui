7e0e7996956dae5d086f60012431a16454af788a
Юрий <bomzheg@gmail.com>
2026-04-10 20:43:15 +0300

    Fix running game route matching before dynamic game id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,6 @@ import {GamePlayComponent} from "./game_play/game_play.component";
 export const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: "games", component: GamesComponent},
-  {path: "games/:id", component: GameComponent},
   {path: "games/running", component: GamePlayComponent},
+  {path: "games/:id", component: GameComponent},
 ];
