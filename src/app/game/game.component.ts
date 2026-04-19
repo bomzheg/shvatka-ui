1ce1517d8d6134614b2c1e7d4040f7e9ec077e1d
Юрий <bomzheg@gmail.com>
2026-04-19 22:36:41 +0300

    Adapt game level card to scenario conditions

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {GameService} from "./game.service";
-import {FullGame, GameStat, HintPart, Keys, Level} from "../domain/game.models";
+import {FullGame, GameStat, HintPart, Keys, Level, ScenarioCondition, ScenarioConditionType} from "../domain/game.models";
 import {ActivatedRoute, ParamMap} from "@angular/router";
 import {HttpAdapter} from "../http/http.adapter";
 import {HintPartComponent} from "../hint.part/hint.part.component";
@@ -18,6 +18,7 @@ import {GameLogPartComponent} from "../game_log.part/game_log.part.component";
   styleUrl: './game.component.scss'
 })
 export class GameComponent implements OnInit, OnDestroy {
+  protected readonly ScenarioConditionType = ScenarioConditionType;
   private routeSubscription: Subscription | undefined;
 
   constructor(
@@ -56,6 +57,44 @@ export class GameComponent implements OnInit, OnDestroy {
 
   getLevels(): Level[] {
     return this.getGame()?.levels ?? [];
+  }
+
+  getScenarioConditions(level: Level): ScenarioCondition[] {
+    return level.scenario.conditions ?? [];
+  }
+
+  getConditionTitle(condition: ScenarioCondition): string {
+    switch (condition.type) {
+      case ScenarioConditionType.winKey:
+        return "Ключ на прохождение";
+      case ScenarioConditionType.effectsKey:
+        return "Ключ с эффектом";
+      case ScenarioConditionType.effectsTimer:
+        return "Таймерный эффект";
+      default:
+        return condition.type;
+    }
+  }
+
+  getConditionEffectsSummary(condition: ScenarioCondition): string[] {
+    return condition.effects.flatMap(effect => {
+      const tags: string[] = [];
+      if (effect.bonus_minutes > 0) {
+        tags.push(`бонус ${effect.bonus_minutes} мин.`);
+      } else if (effect.bonus_minutes < 0) {
+        tags.push(`штраф ${-effect.bonus_minutes} мин.`);
+      }
+
+      if (effect.level_up) {
+        if (effect.next_level) {
+          tags.push(`переход на ${effect.next_level}`);
+        } else {
+          tags.push("переход на следующий уровень");
+        }
+      }
+
+      return tags;
+    });
   }
 
   isLoading(): boolean {
